5a49716de8a5c211e74c0a4656bc08db6c4c3346
Gergely Nagy <algernon@balabit.hu>
2013-11-18 14:57:56 +0100

    debian: Switch to https submodules on bootstrap, on request
    If debian/tools/bootstrap.sh is called with an "https" argument, force
    the submodule to be https instead of git.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- debian/tools/bootstrap.sh
+++ debian/tools/bootstrap.sh
@@ -6,6 +6,12 @@ set -e
 UPSTREAM_VERSION="$(dpkg-parsechangelog | grep '^Version:' | sed -e 's,^Version: ,,' -e 's,\.\(dfsg\|original\),,' -e 's,-[0-9\.]*[~\+]\(.*\),,')"
 UPSTREAM_MAJOR="$(echo ${UPSTREAM_VERSION} | cut -d . -f 1,2)"
 
+if [ "$1" = "https" ]; then
+        sed -e "s#git://#https://#" \
+                < .gitmodules > .gitmodules.new && \
+                mv .gitmodules.new .gitmodules
+fi
+
 git submodule --quiet update --init
 if [ -d syslog-ng-${UPSTREAM_MAJOR} ]; then
         (cd syslog-ng-${UPSTREAM_MAJOR} && git submodule --quiet sync)
